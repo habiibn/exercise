2b4c1e82c84de8405be9559bfbd49b07bca1dcb9
habiibn <habibnurkholis25@gmail.com>
2022-12-01 22:01:11 +0700

    Add biodata

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// still failed :"
-const activeNavbar = document.getElementById("navbar");
-let height = activeNavbar.offsetHeight;
-var navBar = document.getElementsByTagName("nav");
+// // still failed :"
+// const activeNavbar = document.getElementById("navbar");
+// let height = activeNavbar.offsetHeight;
+// var navBar = document.getElementsByTagName("nav");
 
-// When page scrolled down, navigation bar will follow
-if (activeNavbar.scrollTop >= height){
-    navBar.style.position='fixed';
+// // When page scrolled down, navigation bar will follow
+// if (activeNavbar.scrollTop >= height){
+//     navBar.style.position='fixed';
+// }
+
+var tombolMenu = document.getElementsByClassName("three-strips");
+var menu = document.getElementsByClassName("menu");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
 }
+
+document.querySelector(document).ready(function () {
+    var width = document.querySelector(window).width();
+    if (width < 450) {
+        klikMenu();
+    }
+})
